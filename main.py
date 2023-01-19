b65bf479eb3ae6820a4e43159a1d150d67db9b85
ColeD <cole.dylewski@gmail.com>
2023-01-18 20:48:21 -0500

    1/18/2023 - moved player to class

--- main.py
+++ main.py
@@ -1,24 +1,70 @@
 import random
 
 
-#quantum black jack
-#gets the results for all possible decision trees of each hand and records the result
+# quantum black jack
+# gets the results for all possible decision trees of each hand and records the result
+
+class Player:
+
+
+    def __init__(self, name):
+        self.name = 'Player_' + str(name+1)
+        self.status = True
+        self.hand = []
+
+    def __str__(self):
+        print(self.hand)
+
+    def deal_card(self, card):
+        #print(card)
+        self.hand.append(card)
+        #print(self.hand)
+    def new_hand(self):
+        self.hand = []
+
+    def get_hand(self):
+        #print(self.hand)
+        return self.hand
+
 
 def build_deck(numdecks=1):
-    suits = ['C','S','H','D']
-    cards = [i for i in range(2,11)]+['J','Q','K','A']
-    #print(cards)
+    suits = ['C', 'S', 'H', 'D']
+    cards = [i for i in range(2, 11)] + ['J', 'Q', 'K', 'A']
+    # print(cards)
     deck = []
     for d in range(numdecks):
         for s in suits:
             for c in cards:
                 deck.append(f'{s}_{c}')
 
-    print(len(deck))
+    # print(len(deck))
     for i in range(7):
-        print('shuffle')
+        # print('shuffle')
         random.shuffle(deck)
-    #print(deck)
+    # print(deck)
     return deck
-build_deck(7)
-print(364/52)
+
+
+def build_table(numPlayers=1):
+    table = {}
+
+    return table
+
+
+def deal_cards(deck, table):
+    print(deck[:28])
+    for player, hand in table.items():
+        print(player, hand)
+
+
+deck = build_deck(numdecks=7)
+table = build_table(numPlayers=7)
+player_1 = Player(name=0)
+player_1.deal_card('c_j')
+player_1.deal_card('c_k')
+
+hand = player_1.get_hand()
+print(hand)
+player_1.new_hand()
+hand = player_1.get_hand()
+print(hand)
